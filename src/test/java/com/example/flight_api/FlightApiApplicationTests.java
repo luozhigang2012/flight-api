b5c9e2b7875ee9820f0869c940deafc01298975f
Zhigang Luo <lzgluo@cn.ibm.com>
2025-06-11 16:38:49 +0800

    initial commit

---
+++ src/test/java/com/example/flight_api/FlightApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flight_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
